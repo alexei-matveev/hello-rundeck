cd6ab0139d87bc44e105aa6283616f9389878f05
Alexei Matveev <alexei.matveev@gmail.com>
2020-10-07 21:12:12 +0200

    AM: lein new app

---
+++ rundeck-clj-plugin/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rundeck-clj-plugin
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ rundeck-clj-plugin/test/rundeck_clj_plugin/core_test.clj
@@ -0,0 +1,7 @@
+(ns rundeck-clj-plugin.core-test
+  (:require [clojure.test :refer :all]
+            [rundeck-clj-plugin.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
