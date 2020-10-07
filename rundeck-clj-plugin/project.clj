ef866d25702e2ff9d513801ac43f311d33329c61
Alexei Matveev <alexei.matveev@gmail.com>
2020-10-07 22:47:01 +0200

    AM: this is a convention

--- rundeck-clj-plugin/project.clj
+++ rundeck-clj-plugin/project.clj
@@ -12,6 +12,7 @@
              :uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
   ;; https://docs.rundeck.com/docs/developer/01-plugin-development.html#java-plugin-development
+  :uberjar-name "rundeck-clj-plugin-0.1.0.jar"
   :manifest {"Rundeck-Plugin-Version" "1.2"
              "Rundeck-Plugin-Archive" "true"
              "Rundeck-Plugin-Classnames" "rundeck_clj_plugin.core"
